d0f5ef150abc01dc30e427ea2d6365530c49ee8c
Nicholas Easter <nicholas.easter17@gmail.com>
2021-11-29 12:18:44 -0800

    added randomGenerator function to create correct password length

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,19 @@
 // Assignment code here
+function randomGenerator(length) {
+  var pass = Math.random().toString(16).substr(2, length);
+  // if the length of the generated string is less than the
+  // the length the user has specified, make the string longer
+  while(pass.length < length) {
+    var pass = pass +  Math.random().toString(16).substr(2, length - pass.length);
+  }
+  return pass;
+}
+
 function generatePassword() {
   var pwLength = window.prompt("Enter amount of characters(8-128):");
   if (pwLength >= 8 && pwLength <= 128){
-    return "success";
+    // creates a random string equal to the length they inputed
+    return randomGenerator(pwLength);
   } else {
     return "failed";
   }
